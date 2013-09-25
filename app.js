b12e6bcc410aba4453fd8220725c032cbe896b2d
Dav Glass <davglass@gmail.com>
2013-09-25 07:33:21 -0500

    Added support for the combohandler

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 var express = require('express'),
     exphbs  = require('express3-handlebars'),
     helmet = require('helmet'),
+    combo = require('combohandler'),
     app = express();
 
 helmet.defaults(app);
 
 app.use(express['static']('./static'));
+app.get('/combo', combo.combine({rootPath: './static'}), combo.respond);
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+// development only
+app.configure('development', function(){
+    app.locals({
+        development: true
+    });
+});
+
+// production only
+app.configure('production', function(){
+    app.locals({
+        development: false
+    });
+});
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -20,6 +36,18 @@ app.get('/register', function (req, res) {
 
 app.get('/twitter', function (req, res) {
     res.redirect('http://twitter.com/hacksi');
+});
+
+app.get('/github', function (req, res) {
+    res.redirect('https://github.com/HackSI');
+});
+
+app.get('/facebook', function (req, res) {
+    res.redirect('https://www.facebook.com/HackSouthernIllinois');
+});
+
+app.get('/flickr', function (req, res) {
+    res.redirect('http://www.flickr.com/groups/hacksi/');
 });
 
 app.get('/about', function (req, res) {
