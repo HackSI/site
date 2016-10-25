2606ca9262995bc052f7542b111ea8f39f3d81ee
Dav Glass <davglass@gmail.com>
2016-10-25 15:55:15 -0500

    using eslint and esnext features

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-var Hapi = require('hapi'),
-    routes = require('./lib/routes');
+'use strict';
 
-var server = new Hapi.Server();
+const Hapi = require('hapi');
+const routes = require('./lib/routes');
+
+const server = new Hapi.Server();
 server.connection({ host: '0.0.0.0', port: parseInt(process.env.PORT, 10) || 8900 });
 
 server.views({
@@ -16,10 +18,10 @@ server.views({
 });
 
 server.route(routes);
-server.start(function() {
-    console.log('Launched server at', new Date());
-    console.log('Server listening: ' + server.info.uri);
-    console.log('Working env is: ' + process.env.NODE_ENV);
+server.start(() => {
+    console.log(`Launched server at  ${new Date()}`);
+    console.log(`Server listening: ${server.info.uri}`);
+    console.log(`Working env is: ${process.env.NODE_ENV}`);
 });
 
 module.exports = server;
